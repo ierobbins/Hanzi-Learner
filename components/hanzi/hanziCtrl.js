3a2b62e867bdfbb20089a0975c66346b4f2b3dea
Ian Robbins <iedwardrobbins@gmail.com>
2016-09-17 10:23:12 -0500

    dictionary filter works for sound, meaning and radical

--- components/hanzi/hanziCtrl.js
+++ components/hanzi/hanziCtrl.js
@@ -10,11 +10,65 @@ angular.module("hanziLearner")
       $scope.radDisplay = !$scope.radDisplay;
     }
 
-    $scope.displaySearch = function(){
+    $scope.searchReturn = [];
+
+    $scope.selectedRadical = "";
+
+    $scope.selectRadical = function(initRad){
+      $scope.selectedRadical = initRad;
+      $scope.radDisplay = false;
     }
 
-    $scope.searchChars = function(initPin, initDef, initRad){
+    $scope.removeRadical = function(){
+      $scope.selectedRadical = "";
+    }
 
+    //CREATES AND RETURNS AN ARRAY THAT FILTERS OUT ALL ELEMENTS NOT IN THE PARAMETERS
+    $scope.displaySearch = function(initPin, initDef, initRad){
+      var flag = true;
+      $scope.searchReturn = [];
+      $scope.characters.forEach(function(item, index){
+        flag = true;
+        if(initPin){
+          console.log("initPin works");
+          if(!containsString(initPin, item.pinyin)){
+            flag = false;
+          }
+        }
+        if(initDef){
+          console.log("initDef works");
+          if(!containsString(initDef, item.definition)){
+            flag = false;
+          }
+        }
+        if(initRad){
+          console.log("initRad works");
+          if(!containsRadical(item.radical, item.character)){
+            flag = false;
+          }
+        }
+        if(flag){
+          // item.pinyin = item.pinyin[0];
+          $scope.searchReturn.push(item);
+        }
+      });
+
+      //CHECKS IF PINYIN AND DEFINITION SUBSTRINGS EXIST IN EACH ELEMENT OF THE DICTIONARY
+      function containsString(a, b){
+        var indexArr = [];
+        for(var i = 0; i < a.length; i++){
+          indexArr.push(b.indexOf(a.charAt(i)));
+        }
+        for(var ii = 0; ii < indexArr.length - 1; ii++){
+          if((indexArr[ii] + 1) !== indexArr[ii + 1]){
+            return false;
+          }
+        }
+        return true;
+      }
+      function containsRadical(radical, character){
+        return initRad === radical || initRad === character;
+      }
     }
 
     $scope.radicals = hanziSrv.getRadicals();
